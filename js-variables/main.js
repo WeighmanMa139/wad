5839dbdd85212ed72a6496ea47246484681b55be
Maximus Weighman <WeighmanMa139@northwested.org>
2024-04-15 13:49:37 -0400

    update

---
+++ js-variables/main.js
@@ -0,0 +1,17 @@
+// Max W.
+// 4/15/24
+// js-varibles 
+
+// imput
+const numInches = 78;
+const conversionFactor = 2.54;
+
+
+//prosess
+const numCentimeters = numInches * conversionFactor;
+
+//Output 
+
+const myOutput = `${numInches} inches is equivalent to ${numCentimeters} centimeters.`;
+
+console.log (myOutput);
