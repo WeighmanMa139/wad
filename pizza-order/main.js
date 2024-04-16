a6e3b73dda34eadef56fe670a4bf271bbbd9f29f
Maximus Weighman <WeighmanMa139@northwested.org>
2024-04-16 13:22:52 -0400

    update

---
+++ pizza-order/main.js
@@ -0,0 +1,19 @@
+// Max W.
+// 4/15/24
+// Java Intro
+
+
+//imput
+
+let size = prompt("Enter size of pizza (medeum, small, large");
+
+let topping1 = prompt("Enter first topping:")
+let topping2 = prompt("Enter second topping:")
+
+//prosess
+let msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
+
+
+
+//output
+alert(msg);
