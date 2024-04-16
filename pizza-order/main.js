d9ecd9fd31ba53e824c81c305242b87f70c71aa7
Maximus Weighman <WeighmanMa139@northwested.org>
2024-04-16 13:38:34 -0400

    new update

--- pizza-order/main.js
+++ pizza-order/main.js
@@ -2,6 +2,9 @@
 // 4/15/24
 // Java Intro
 
+document.getElementById("btn").addEventListener("click", placeOrder);
+
+function placeOrder(){
 
 //imput
 
@@ -11,9 +14,13 @@ let topping1 = prompt("Enter first topping:")
 let topping2 = prompt("Enter second topping:")
 
 //prosess
+
 let msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
 
 
 
 //output
+
 alert(msg);
+
+}
