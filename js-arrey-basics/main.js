672751c5c1ca9624316f3c97cb3e3fb1a4c54028
Maximus Weighman <WeighmanMa139@northwested.org>
2024-04-29 13:50:49 -0400

    new project

---
+++ js-arrey-basics/main.js
@@ -0,0 +1,36 @@
+// Max W.
+// 4/29/24
+// js arrey
+
+// Arrey
+const dragonBall = ["Goku", "Vegeta", "Gohan", "Piccolo"];
+
+// name
+console.log (`***This code was writen by Maximus Weighman***`);
+
+console.log(`\n`);
+
+// origanal
+console.log (dragonBall)
+console.log (`Laingth of DragonBall array: 4 elements`);
+
+
+// push()
+console.log(`\n`);
+
+console.log (`Adding Trunks useing the push() method`);
+
+dragonBall.push(`Trunks`);
+
+console.log (dragonBall)
+
+// pop()
+
+console.log (`Removeing trunks useing the pop() method`);
+
+dragonBall.pop()
+
+console.log (dragonBall)
+
+
+
